18dfde95ecacf0c3abbc8288a4856f3bd4556d1f
Andy Strube <andystrube64@gmail.com>
2021-07-06 14:46:33 -0700

    bug fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:post]
+    origins 'https://andy-todo-list-front-end.herokuapp.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:post]
   end
 end
-
-Rails.application.config.hosts << "http://localhost:8000/"
-Rails.application.config.hosts << "https://andy-todo-list-front-end.herokuapp.com/"
