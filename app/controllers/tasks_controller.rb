dd026b6f0aecd1307a0a829f6ec21d87fdc51767
Andy Strube <andystrube64@gmail.com>
2021-07-05 17:17:19 -0700

    Added rack-cors gem

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def create
-    task = Task.create(task_prams)
+    task = Task.create(task_params)
   end
 
   private
