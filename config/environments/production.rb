e060b16f639971784f3855f6e3ad5ef7be2384b4
Andy Strube <andystrube64@gmail.com>
2021-07-06 12:40:58 -0700

    refactor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "https://andy-todo-list-front-end.herokuapp.com/"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
